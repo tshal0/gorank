cdd18eb29aab8a5e80ec20f7e0a950ceb6c80d41
valkn0t <twshal@gmail.com>
2018-01-16 19:13:28 -0600

    finalized MergeSort inversion counting solution

--- MergeSort.go
+++ MergeSort.go
@@ -4,16 +4,22 @@
 
 // TODO:
 
+// Create DataSet struct
+// Add ScanInts functionality for scanning in space delimited integers.
+//
+
 package gorank
 
 // import _ "container/heap"
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 // import _ "bufio"
 // import _ "os"
-import "bufio"
-import "os"
-import "strings"
 
 // MergeSortFunc should perform MergeSort alg for hackerrank
 func MergeSortFunc() {
@@ -26,30 +32,31 @@ func MergeSortFunc() {
 		fmt.Println("Err")
 		return
 	}
-
+	invCounts := make([]uint64, d)
+	datasets := make([]MSDataSet, d)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Split(bufio.ScanWords)
 	for i := 0; i < d; i++ {
 		var ds MSDataSet
-		if _, err := fmt.Scanf("%d\n", &ds.size); err != nil {
-			fmt.Println("Err")
-			return
-		}
-
-		ScanInts(ds.size, &ds.data)
-
-		fmt.Println(ds.data)
+		ds.ScanInts(scanner)
+		datasets[i] = ds
+	}
+	for i, ds := range datasets {
+		inv := ds.MergeSort(0, ds.size-1)
+		invCounts[i] = inv
 	}
 
-}
-
-// MergeSort performs merge sort sorting algorithm
-func MergeSort() {
-
+	for _, val := range invCounts {
+		fmt.Println(val)
+	}
 }
 
 // MSDataSet data structure stores merge sort data set
+// Might actually begin to use this for all int sets.
 type MSDataSet struct {
-	data []int
-	size int
+	data      []int
+	workspace []int
+	size      int
 }
 
 func toInt(buf []byte) (n int) {
@@ -60,16 +67,72 @@ func toInt(buf []byte) (n int) {
 }
 
 // ScanInts scans in a space delimited set of ints
-func ScanInts(size int, values *[]int) {
+func (ds *MSDataSet) ScanInts(scanner *bufio.Scanner) {
+	// Get dataset size
+	scanner.Scan()
+	ds.size = toInt(scanner.Bytes())
+	// Init scanner
 
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Split(bufio.ScanWords)
-	v := *values
-	for i := 0; i < size; i++ {
+	// Init dataset to be size d
+	ds.data = make([]int, ds.size)
+	ds.workspace = make([]int, ds.size)
+	for i := 0; i < ds.size; i++ {
 		scanner.Scan()
-		v = append(v, toInt(scanner.Bytes()))
+		ds.data[i] = toInt(scanner.Bytes())
 	}
 
+}
+
+// MergeSort performs merge sort sorting algorithm
+// Takes starting and ending points, ran on original dataset.
+func (ds *MSDataSet) MergeSort(start, end int) (inversions uint64) {
+	if start == end {
+		return 0
+	}
+
+	mid := (start + end) / 2
+
+	inversions += ds.MergeSort(start, mid) // Left
+	inversions += ds.MergeSort(mid+1, end) // Right
+	inversions += ds.Merge(start, end, mid)
+
+	return inversions
+}
+
+//Merge sort
+func (ds *MSDataSet) Merge(start, end, mid int) (inversions uint64) {
+
+	newArray := make([]int, (end - start + 1))
+
+	curr, i, j := 0, start, mid+1
+
+	for i <= mid && j <= end {
+		if ds.data[i] > ds.data[j] {
+			newArray[curr] = ds.data[j]
+			curr++
+			j++
+			inversions += uint64(mid - i + 1)
+		} else {
+			newArray[curr] = ds.data[i]
+			curr++
+			i++
+		}
+	}
+	for i <= mid {
+		newArray[curr] = ds.data[i]
+		curr++
+		i++
+	}
+	for j <= end {
+		newArray[curr] = ds.data[j]
+		curr++
+		j++
+	}
+
+	dsa := ds.data[start:(start + (end - start) + 1)]
+
+	copy(dsa, newArray)
+	return inversions
 }
 
 // Custom split func I found
